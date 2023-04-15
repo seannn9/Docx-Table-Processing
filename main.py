df115bc07b55b1a6e5d484259a350af6eab77af8
seannn9 <naeskirlu9@gmail.com>
2023-04-15 21:56:43 +0800

    code refactor for easier modification

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import docx
 import os
 
-file_name = "newtables.docx"
+file_name = ".docx" # file to modify
 path = "Downloads\\"
 dl = os.path.join(os.path.expanduser("~"), path)
 doc = docx.Document(dl+file_name)
 
+# can modify if necessary
 start = 1
-stop = 2 # add 1 to the actual stopping point
 curr_stop = 1
-num = "0"
+num = "0" # element to place inside table cell
 intstate = 0
-column = 3 # specify which column to edit, starting with 0
-row_size = 17 # add 1 to the actual column size
-steps = 1 #
-table1 = doc.tables[2] # specify what table to edit, starting with 0
 
-while stop != row_size:
+# variables to modify
+stop = 2 # add 1 to the actual stopping point (steps + 1)
+column = 0 # specify which column to edit, starting with 0
+column_size = 0 # add 1 to the actual column size
+steps = 1 # how many iterations before changing num
+table1 = doc.tables[0] # specify what table to edit, starting with 0
+
+while stop != column_size:
     if curr_stop == stop:
         start += steps
         stop += steps
