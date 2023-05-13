479f5833bfda36cc53651a8dacec0ccbb48607aa
Sean Ulric <seanulric@Seans-MacBook-Air.local>
2023-05-13 23:13:19 +0800

    Modified variables

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import docx
 import os
 
-file_name = ".docx" # file to modify
-path = "Downloads\\" # change for different file location
+file_name = "montanoCPE21.docx" # file to modify
+path = "Downloads/" # change for different file location
 dl = os.path.join(os.path.expanduser("~"), path)
 doc = docx.Document(dl+file_name)
 
 # can modify if necessary
 start = 1
 curr_stop = 1
-num = "0" # element to place inside table cell
 intstate = 0
 
 # variables to modify
-table1 = doc.tables[0] # specify what table to edit, starting with 0
-column = 0 # specify which column to edit, starting with 0
-column_size = 0 # add 1 to the actual column size
+num = "0" # element to place inside table cell
+table1 = doc.tables[8] # specify what table to edit, starting with 0
+column = 3 # specify which column to edit, starting with 0
+column_size = 17 # add 1 to the actual column size if there's a header
 stop = 2 # add 1 to the actual stopping point (steps + 1)
 steps = 1 # how many iterations before changing num
 
