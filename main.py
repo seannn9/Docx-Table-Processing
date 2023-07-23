e50e602004e160ce5496582787ce5663858833fa
seannn9 <naeskirlu9@gmail.com>
2023-07-23 14:37:25 +0800

    Refactored comments

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import docx
 import os
 
-file_name = ".docx" # file to modify
+# Edit docx tables that contains patterns
+
+file_name = "" # file to modify
 path = "Downloads\\" # change for different file location
 dl = os.path.join(os.path.expanduser("~"), path)
 doc = docx.Document(dl+file_name)
 
 # can modify if necessary
-start = 1
+start = 1 # specify which row to start with
 curr_stop = 1
 num = "0" # element to place inside table cell
 intstate = 0
 
 # variables to modify
-table1 = doc.tables[0] # specify what table to edit, starting with 0
+table1 = doc.tables[10] # specify what table to edit, starting with 0
+column_size = 17
 column = 0 # specify which column to edit, starting with 0
-column_size = 0 # add 1 to the actual column size
-stop = 2 # add 1 to the actual stopping point (steps + 1)
-steps = 1 # how many iterations before changing num
+steps = 8 # how many iterations before changing num
+stop = 9 # add 1 to the actual stopping point (steps + 1)
+
 
 while stop != column_size:
     if curr_stop == stop:
@@ -27,7 +30,7 @@ while stop != column_size:
     if intstate % 2 == 0:
         num = "0"
     else:
-        num = "1"
+        num = "1" 
     for row in range(start, stop):
         table1.cell(row, column).text = num
         curr_stop += 1
